aad9dbe4c2858a7436a3ea75cf744ee60a37f2f4
huruizverastegui <118810504+huruizverastegui@users.noreply.github.com>
2024-06-13 15:49:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
+
 import streamlit as st
+
+st.set_page_config(
+    page_title="Talk to CPD",
+    page_icon="👋",
+)
+st.sidebar.success("Select a page above.")
 
 st.header("Talk to CPD")
 st.write("Select a page from the sidebar to get started")
