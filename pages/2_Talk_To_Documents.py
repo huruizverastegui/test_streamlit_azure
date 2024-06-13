4abf89b9c886f2131e854a0d454401bc92fbbcb7
huruizverastegui <118810504+huruizverastegui@users.noreply.github.com>
2024-06-13 17:04:42 +0700

    Create 2_Talk_To_Documents.py

---
+++ pages/2_Talk_To_Documents.py
@@ -0,0 +1,22 @@
+
+import streamlit as st
+import openai
+import llama_index
+from llama_index.llms.openai import OpenAI
+try:
+  from llama_index import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
+except ImportError:
+  from llama_index.core import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
+
+from azure.storage.blob import BlobServiceClient
+from io import BytesIO
+
+from llama_index.core.extractors import (
+    TitleExtractor,
+    QuestionsAnsweredExtractor,
+)
+from llama_index.core.node_parser import TokenTextSplitter
+
+
+st.header("Page 2")
+st.write("blablabli")
